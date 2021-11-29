de65e02bdd76a7022c22730019eaaa15d868199b
Rose <roset@roses-air.home>
2021-11-29 10:06:18 +0000

    basic building form structure

---
+++ frontEnd/moop_city_front/src/components/BuildingForm.js
@@ -0,0 +1,75 @@
+import {useState} from 'react';
+
+const BuildingForm = ( {onFormSubmission}) => {
+
+    const [buildingType, setBuildingType] = useState("");
+    const [buildingName, setBuildingName] = useState("");
+    const [capacity, setCapacity] = useState("");
+    const [allotmentID, setAllotmentID] = useState(0);
+
+    const handleBuildingTypeChange = (event) => {
+        setBuildingType(event.target.value);
+    }
+
+    const handleBuildingNameChange = (event) => {
+        setBuildingName(event.target.value);
+    }
+
+    const handleCapacityChange = (event) => {
+        setCapacity(event.target.value);
+    }
+
+    const handleAllotmentIDChange = (event) =>{
+        setAllotmentID(event.target.value);
+    }
+
+    const handleFormSubmission = (event) => {
+        event.preventDefault();
+
+        const newBuilding = {
+            buildingType: buildingType,
+            buildingName: buildingName,
+            capacity: capacity,
+            allotmentID: allotmentID
+        }
+
+        onFormSubmission(newBuilding);
+
+        setBuildingType("");
+        setBuildingName("");
+        setCapacity("");
+        setAllotmentID(0);
+    }
+
+    return (
+        <>
+            <h2>Add a new building:</h2>
+            <form onSubmit={handleFormSubmission}>
+                <div className="formElement">
+                    <label htmlFor="buildingType">Choose building type: </label>
+                    <select name="buildingType" id="buildingType">
+                        <option value="house">House</option>
+                        <option value="workplace">Workplace</option>
+                    </select>
+                </div>
+                <div className="formElement">
+                    <label htmlFor="buildingName">Building Name: </label>
+                    <input type="text" id="buildingName" value={buildingName} onChange={handleBuildingNameChange}/>
+                </div>
+                <div className="formElement">
+                    <label htmlFor="capacity">Capacity: </label>
+                    <input type="text" id="capacity" value={capacity} onChange={handleCapacityChange}/>
+                </div>
+                <div className="formElement">
+                    <label htmlFor="allotmentID">Allotment Coordinates: </label>
+                    <input type="text" id="allotmentID" value={allotmentID} onChange={handleAllotmentIDChange}/>
+                </div>
+                <div className="formElement">
+                    <input type="submit" value="Add Task"/>
+                </div>
+            </form>
+        </>
+    )
+        
+    
+}
